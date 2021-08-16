66dd22b26efc5a2f0c01def16b12ca994d01f137
Trevor Sanderson <tjsande@umich.edu>
2021-08-16 11:36:05 -0400

    updated gitignore / removed old resume

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.703f48cbbafa889f8ef1e0802108fde9.js"
+  "/precache-manifest.dbd1888e68c23ebffa053880aeb93338.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
